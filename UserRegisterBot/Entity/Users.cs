b7f7c597caa449046f556490cfcc6d4d6cfefd77
SamandarbekYR <SamandarbekYR@gmail.com>
2023-10-31 04:42:16 +0500

    Fixed text

--- UserRegisterBot/Entity/Users.cs
+++ UserRegisterBot/Entity/Users.cs
@@ -3,6 +3,7 @@
 public class Users
 {
     public long Id { get; set; }
+    public long ChatId { get; set; }
     public string UserName { get; set; } = string.Empty;
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
